63f80d3336c4eb8545ebe65bd6f9a6a9ae1c15c0
Sandynoob <33446334+Sandynoob@users.noreply.github.com>
2025-12-27 20:28:49 +0800

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,27 +1,20 @@
 
-export enum GameState {
-  START = 'START',
-  PLAYING = 'PLAYING',
-  FINISHED = 'FINISHED'
-}
-
-export interface Word {
-  word: string;
-  lexile: number;
-  definition?: string;
-}
-
-export enum LexileRange {
-  BR_200L = 'BR-200L',
-  L200_400 = '200L-400L',
-  L400_600 = '400L-600L',
-  L600_800 = '600L-800L',
-  L800_1000 = '800L-1000L',
-  L1000_PLUS = '1000L+'
-}
-
-export interface ScoreData {
-  totalScore: number;
-  correctFirstTry: number;
-  totalWords: number;
+{
+  "compilerOptions": {
+    "target": "ESNext",
+    "module": "ESNext",
+    "lib": ["DOM", "DOM.Iterable", "ESNext"],
+    "allowJs": true,
+    "skipLibCheck": true,
+    "esModuleInterop": true,
+    "allowSyntheticDefaultImports": true,
+    "strict": true,
+    "forceConsistentCasingInFileNames": true,
+    "moduleResolution": "node",
+    "resolveJsonModule": true,
+    "isolatedModules": true,
+    "noEmit": true,
+    "jsx": "react-jsx"
+  },
+  "include": ["**/*.ts", "**/*.tsx"]
 }
